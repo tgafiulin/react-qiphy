5d8bdb5e2e43ef1f335106ff57919dcab89bd406
tgafiulin <t.gafiulin@ya.ru>
2021-01-31 13:02:54 +0300

    add GIF Output

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
 import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.scss';
 import { Button } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
-import { fetchGiphs } from './app/tagReducer'
+import { fetchGiphs } from './app/giphsReducer'
+import GiphItem from './comnponents/GiphItem'
 
 function App() {
   const dispatch = useDispatch();
   const [tags, editTagsInput] = useState('');
   const [tagsError, editTagsError] = useState('');
-  // const giphs = useSelector(state => state.tags.giphs)
+  const giphs = useSelector((state) => state.giphs.giphs)
 
-  const getGiphs = () => {
-    
-    
+  const getGiphs = () => {   
     if (tags) {
       dispatch(fetchGiphs(tags));
-      editTagsInput('');
+      // editTagsInput('');
       editTagsError('');
     } else {
       editTagsError('заполните поле «тег»');
@@ -30,20 +28,28 @@ function App() {
 
   return (
     <div className="App">
-      <div className="input-block">
-        <input 
-          type="text"
-          value={tags}
-          onChange={(e) => editTagsInput(e.target.value)}
-          placeholder="введите тег"
-          className="form-control"
-        />
-        <span>{tagsError}</span>
+      <div className="search-block">
+        <div className="input-block">
+          <input 
+            type="text"
+            value={tags}
+            onChange={(e) => editTagsInput(e.target.value)}
+            placeholder="введите тег"
+            className="form-control"
+          />
+          <span>{tagsError}</span>
+        </div>
+        
+        <Button variant="success" onClick={getGiphs}>Загрузить</Button>
+        <Button variant="danger" onClick={clear}>Очистить</Button>
+        <Button variant="primary">Группировать</Button>
       </div>
-      
-      <Button variant="success" onClick={getGiphs}>Загрузить</Button>
-      <Button variant="danger" onClick={clear}>Очистить</Button>
-      <Button variant="primary">Группировать</Button>
+      <div className="result-block">
+        {giphs.map((gif) => {
+          return <GiphItem gif={gif} />
+          })
+        }
+      </div>
     </div>
   );
 }
